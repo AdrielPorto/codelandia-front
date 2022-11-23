e6ac7b9e6c9ae6ff3bd82fd568d6385b856b5b50
Adriel Porto <adriel.porto@dmslog.com>
2022-11-23 01:55:15 -0300

    teste

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 export const api = axios.create({
     baseURL: 'http://44.202.38.56:3000',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    },
 });
+
